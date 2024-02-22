03dfc957db68c7e5737b10248dd1c278134afe1c
Moraesdv <viniciushenrrique142@gmail.com>
2024-02-21 22:53:07 -0300

    Exercício 0010

---
+++ Exercícios/Ex0010/index02.js
@@ -0,0 +1,22 @@
+var hora = 
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if(hora<5){
+    console.log('Boa Madrugada!')
+} else{
+    if(hora<12){
+        console.log('Bom Dia!')
+        
+    } else{
+        if(hora == 12){
+            console.log('Bom meio-dia')
+        } else{
+            if(hora<18){
+                console.log('Boa Tarde!')
+            } else{
+                console.log('Boa Noite!')
+            }
+        }
+    } 
+    
+}
