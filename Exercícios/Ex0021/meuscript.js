20255875eec6af763399ec6b80fef99a472fce51
Moraesdv <viniciushenrrique142@gmail.com>
2024-03-07 22:43:14 -0300

    Seletor de cores

---
+++ Exercícios/Ex0021/meuscript.js
@@ -0,0 +1,10 @@
+document.getElementById('laran').addEventListener("click", function(){
+    document.querySelector('body').setAttribute("class", "btn-laranja");
+});
+
+document.getElementById('verme').addEventListener("click", function(){
+    document.querySelector('body').setAttribute("class", "btn-vermelho");
+});
+document.getElementById('ro').addEventListener("click", function(){
+    document.querySelector('body').setAttribute("class", "btn-roxo");
+});
