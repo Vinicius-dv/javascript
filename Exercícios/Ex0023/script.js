d590f4a107adda0826d9089907e8542d96e54a4e
Moraesdv <viniciushenrrique142@gmail.com>
2024-03-12 22:25:04 -0300

    Exercicios

---
+++ Exercícios/Ex0023/script.js
@@ -0,0 +1,10 @@
+/*for(var c = 1; c++;){
+    console.log(`Vamos contar ${c}`)
+}
+*/
+
+//Para contar quantos números eu quiser eu posso usar o método cima , para contar de 1 em um é o método abaixo//
+
+for(var cont = 1; cont<=10; cont++ ){
+    console.log(`Vamos contar ${cont}`)
+}
