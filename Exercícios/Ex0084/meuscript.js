bb4101a7e1081968ed7d54009f204f10af281f95
Moraesdv <viniciushenrrique142@gmail.com>
2024-05-03 00:00:59 -0300

    oiii

--- Exercícios/Ex0084/meuscript.js
+++ Exercícios/Ex0084/meuscript.js
@@ -1,55 +1,64 @@
-class carro{
-    constructor(tipo , estagioTurbo){
-        this.turbo = new turbo(estagioTurbo)
-        if(tipo ==1){
-            this.velMax = 120
-        } else{
-            if(tipo ==2){
-                this.velMax = 160
-            } else{
-                if(tipo ==3){
-                    this.velMax == 200
-            }
-            } 
-            } 
-            this.velMax += this.turbo.potencia
-            }
+class carroPadrão {
+    constructor() {
 
-            info(){
-                console.log(this.velMax)
-                console.log(this.turbo)
-            }
+        if(this.constructor === carroPadrão){
+            throw new  TypeError('Essa classe não pode ser instanciada')
+        }
+        
+        this.rodas = 4;
+        this.portas = 4;
+        this.ligado = false;
+    }
 }
 
-class turbo{
-    constructor(pevent){
-        if(pevent == 1){
-            this.potencia = 50
-        } else{
-            if(pevent ==2){
-                this.potencia = 75
-            } else{
-                if(pevent ==3){
-                    this.potencia = 100
-                }
-            }
+class carro extends carroPadrão {
+    constructor(tipo, estagioTurbo) {
+        super();
+        this.turbo = new turbo(estagioTurbo);
+        if (tipo === 1) {
+            this.velMax = 120;
+        } else if (tipo === 2) {
+            this.velMax = 160;
+        } else if (tipo === 3) {
+            this.velMax = 200;
+        }
+        this.velMax += this.turbo.potencia;
+    }
+
+    info() {
+        console.log(this.velMax);
+        console.log(this.turbo);
+        console.log(this.rodas);
+        console.log(this.portas);
+        console.log(this.ligado);
+    }
+}
+
+class turbo {
+    constructor(pevent) {
+        if (pevent === 1) {
+            this.potencia = 50;
+        } else if (pevent === 2) {
+            this.potencia = 75;
+        } else if (pevent === 3) {
+            this.potencia = 100;
         }
     }
 }
 
-class carroEspecial extends carro{
-    constructor(estagioTurbo){
-        super(estagioTurbo)
-        this.nome = 'Carro especial'
+class carroEspecial extends carro {
+    constructor(tipo, estagioTurbo) {
+        super(tipo, estagioTurbo);
+        this.nome = 'Carro especial';
     }
 }
 
+const c1 = new carro(1, 2);
+const c2 = new carroEspecial(3, 3);
+const c3 = new carroPadrão()
 
 
-const c1 = new carro(1,2)
+c1.info();
+c2.info();
 
-// eu não preciso passar o tipo pq ja passei em super só preciso passar o estagio turbo que eu não passei ainda
-const c2 = new carroEspecial(3)
 
-c1.info()
-c2.info()
