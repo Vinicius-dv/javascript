281f6f81bfc96316fb12f3e7ac40989501c6baa5
Moraesdv <viniciushenrrique142@gmail.com>
2024-05-25 23:06:53 -0300

    Bolinhas

---
+++ Exercícios/Ex0092/meuscript.js
@@ -0,0 +1,55 @@
+const num_objetos = document.getElementById('num_objetos')
+const txt_qtde = document.getElementById('txt-qtde')
+const btn_add = document.getElementById('btn-add')
+const btn_remover = document.getElementById('btn-remover')
+const palco = document.getElementById('palco')
+
+let larguraPalco = palco.offsetWidth
+let alturaPalco = palco.offsetHeight
+let bolas = []
+let numBola = 0
+
+class Bola{
+    constructor(arrayBolas,Palco){
+        this.tam = Math.floor(Math.random()*15)+10
+        this.r = Math.floor(Math.random()*255)
+        this.g = Math.floor(Math.random()*255)
+        this.b = Math.floor(Math.random()*255)
+        this.px = Math.floor(Math.random()*(larguraPalco-this.tam))
+        this.py = Math.floor(Math.random()*(alturaPalco-this.tam))
+        this.velx = Math.floor(Math.random()*2)+0,5
+        this.vely = Math.floor(Math.random()*2)+0,5
+        this.dirx = (Math.random()*10)>5?1:-1
+        this.diry = (Math.random()*10)>5?1:-1
+        this.palco = palco
+        this.arrayBolas = arrayBolas
+        this.id =  Date.now()+'_'+Math.floor(Math.random()*10000000000000)
+        this.desenhar()
+        this.controle = setInterval(this.controlar,10)
+        this.eu =  document.getElementById(this.id)
+
+}
+minhaPos = () =>{
+
+}
+removerBola = ()=>{
+
+}
+desenhar = ()=>{
+
+}
+controlar = () =>{
+
+}
+}
+
+btn_add.addEventListener('click',(evt)=>{
+    const qtd = txt_qtde.value
+    for(let i = 0;i<qtd;i++){
+        //Instanciar novas bolinhas
+    
+    }
+})
+btn_remover.addEventListener('click',(evt)=>{
+    bolas.length = 0
+})
