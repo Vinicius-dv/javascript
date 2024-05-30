426252829f7c4a982e4e64ca7489eb7bc622a966
Moraesdv <viniciushenrrique142@gmail.com>
2024-05-30 20:33:23 -0300

    bolinhas

--- Exercícios/Ex0092/meuscript.js
+++ Exercícios/Ex0092/meuscript.js
@@ -17,8 +17,8 @@ class Bola{
         this.b = Math.floor(Math.random()*255)
         this.px = Math.floor(Math.random()*(larguraPalco-this.tam))
         this.py = Math.floor(Math.random()*(alturaPalco-this.tam))
-        this.velx = Math.floor(Math.random()*2)+0,5
-        this.vely = Math.floor(Math.random()*2)+0,5
+        this.velx = Math.floor(Math.random()*2)+0.5
+        this.vely = Math.floor(Math.random()*2)+0.5
         this.dirx = (Math.random()*10)>5?1:-1
         this.diry = (Math.random()*10)>5?1:-1
         this.palco = palco
@@ -28,7 +28,7 @@ class Bola{
         this.controle = setInterval(this.controlar,10)
         this.eu =  document.getElementById(this.id)
         numBola++
-        num_objetos = numBola
+        num_objetos.innerHTML = numBola
 
 }
 
@@ -45,28 +45,57 @@ removerBola = ()=>{
     })
     this.eu.remove()
     numBola--
-    num_objetos = numBola
+    num_objetos.innerHTML = numBola
 }
 desenhar = ()=>{
     const div = document.createElement('div')
     div.setAttribute('id',this.id)
     div.setAttribute('class','bola')
-    div.setAttribute('style',`left:${this.px}; top:${this.py}; ${this.tam};heigth:${this.tam};background-color: rgb(${this.r},${this.g},${this.b})`)
+    div.setAttribute('style',`left:${this.px}px; top:${this.py}px; width:${this.tam}px;height:${this.tam}px;background-color: rgb(${this.r},${this.g},${this.b})`)
     this.palco.appendChild(div)
 }
-controlar = () =>{
+
+controle_Bordas = () =>{
+    if(this.px+this.tam >= larguraPalco){
+        this.dirx = -1
+    }else if(this.px <= 0){
+        this.dirx = 1
+    }
+    if(this.py+this.tam >= alturaPalco){
+        this.diry = -1
+    }else if(this.py <= 0){
+        this.diry = 1
+    }
 
 }
+
+
+controlar = () =>{
+    this.controle_Bordas()
+    this.px += this.dirx*this.velx
+    this.py += this.diry*this.vely
+    this.eu.setAttribute('style',`left:${this.px}px; top:${this.py}px; width:${this.tam}px;height:${this.tam}px;background-color: rgb(${this.r},${this.g},${this.b})`)
+    if((this.px > larguraPalco) || (this.py> alturaPalco)){
+        this.removerBola()
+     }
+    }
 }
+
+window.addEventListener('resize',()=>{
+    larguraPalco = palco.offsetWidth
+    alturaPalco = palco.offsetHeight
+})
 
 btn_add.addEventListener('click',(evt)=>{
     const qtd = txt_qtde.value
     for(let i = 0;i<qtd;i++){
-        //Instanciar novas bolinhas
+        bolas.push(new Bola(bolas,palco))
     
     }
 })
 btn_remover.addEventListener('click',(evt)=>{
-    bolas.length = 0
+    bolas.map((b)=>{
+        b.removerBola()
+    })
 })
 
