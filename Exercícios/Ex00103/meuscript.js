daa814a835c328f1628bff82c14aff60033cc31c
Moraesdv <viniciushenrrique142@gmail.com>
2024-06-16 22:45:18 -0300

    Symbol

---
+++ Exercícios/Ex00103/meuscript.js
@@ -0,0 +1,12 @@
+const s1 = Symbol()
+const s2 = Symbol()
+const s3 = Symbol.for('Bruno')
+const s4 = Symbol.for('Canal')
+
+
+console.log(s1)
+console.log(s2)
+console.log(s3===s4)
+
+console.log(Symbol.keyFor(s3))
+console.log(Symbol.keyFor(s4))
