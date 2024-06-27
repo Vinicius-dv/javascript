fa1941da86592f9b03efd3805b2e2a2c62d98d52
Moraes dv <150695214+Moraesdv@users.noreply.github.com>
2024-06-27 14:44:17 -0300

    Gerador

---
+++ Exercícios/Ex00110/meuscript.js
@@ -0,0 +1,52 @@
+const slider = document.getElementById('slider')
+let btn_senha = document.getElementById('btn-senha')
+
+let valor = document.getElementById('valor')
+let password = document.getElementById('password')
+
+const container_password = document.getElementById('container-password')
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@'
+
+let novaSenha = ''
+
+valor.innerHTML = slider.value
+
+slider.oninput = function(){
+    valor.innerHTML = this.value
+
+}
+
+function generatePassword(){
+    let pass = ''
+    for(let i =0 ,n=charset.length;i<slider.value;i++){
+        pass += charset.charAt(Math.floor(Math.random()*n))
+       
+
+    }
+    container_password.classList.remove('hide')
+    password.innerHTML = pass
+    novaSenha = pass
+}
+
+btn_senha.addEventListener('click',()=>{
+    generatePassword()
+})
+
+function copy(){
+    navigator.clipboard.writeText(novaSenha)
+}
+
+container_password.addEventListener('click',()=>{
+    copy()
+    alert('Senha copiada com sucesso')
+})
+
+
+
+
+
+
+
+
+
