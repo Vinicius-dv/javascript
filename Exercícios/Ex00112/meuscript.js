48307134900d3eccacf38c179846a689b99af5b2
Moraes dv <150695214+Moraesdv@users.noreply.github.com>
2024-06-29 15:16:32 -0300

    praticando

---
+++ Exercícios/Ex00112/meuscript.js
@@ -0,0 +1,26 @@
+const q1 = document.getElementById('q1')
+const q2 = document.getElementById('q2')
+const posx = document.getElementById('posx')
+const posy = document.getElementById('posy')
+const largura = document.getElementById('largura')
+const altura = document.getElementById('altura')
+
+let Domrect_q1 = q1.getBoundingClientRect()
+let Domrect_q2 = q2.getBoundingClientRect()
+
+q1.addEventListener('click',()=>{
+    posx.innerHTML = `Posx:${Domrect_q1.x}`
+    posy.innerHTML = `Posy:${Math.floor(Domrect_q1.y)}`
+    largura.innerHTML = `Largura:${Domrect_q1.width}`
+    altura.innerHTML = `Altura:${Domrect_q1.height}`
+
+})
+
+
+q2.addEventListener('click',()=>{
+    posx.innerHTML = `Posx:${Domrect_q2.x}`
+    posy.innerHTML = `Posy:${Math.floor(Domrect_q2.y)}`
+    largura.innerHTML = `Largura:${Domrect_q2.width}`
+    altura.innerHTML = `Altura:${Domrect_q2.height}`
+
+})
