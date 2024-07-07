5a7b98b421a120664eb2cb4d7d90c8258a47e724
Moraes dv <150695214+Moraesdv@users.noreply.github.com>
2024-07-07 00:44:54 -0300

    header 80% pronto

--- Exercícios/Ex00113/meuscript.js
+++ Exercícios/Ex00113/meuscript.js
@@ -1,13 +1,36 @@
-let numeros = ()=>{
-    return [10,20,30,40]
-}
-
-let [a,b,c,d] = numeros()
-
+let numeros = [10,20,30,40,50,60,70,80,90]
+let [a,b,c,...d] = numeros
 console.log(a,b,c,d)
+
+
+let obj = {nome:'Bruno',curso:'javascript',canal:'CFBcursos'}
+let {nome,curso} = obj
+
+console.log(nome,curso)
+
+const cores = ()=>{
+    return ['verde','vermelho','azul','roxo']
+}
+let [c1,c2,,c3]=cores()
+console.log(c1,c2,c3)
+
+
+let texto = 'Curso de javascript'
+let [p1,p2,p3]= texto.split(' ')
+console.log(p1,p2,p3)
+
+
+
+
+
+
+
+
+
+/*
 
 const formatar = Intl.NumberFormat('en',{
     notation:'compact'
 })
 
-console.log(formatar.format(123456))
+console.log(formatar.format(123456))*/
