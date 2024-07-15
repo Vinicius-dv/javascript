1ca28cb3c8f818b18a9a8b56dccc122f65a150a2
Moraes dv <150695214+Moraesdv@users.noreply.github.com>
2024-07-15 16:50:25 -0300

    Validação nativa

---
+++ Exercícios/Ex00116/meuscript.js
@@ -0,0 +1,14 @@
+const f_nome = document.getElementById('f_nome')
+const f_nota = document.getElementById('f_nota')
+const f_msg = document.getElementById('f_msg')
+
+document.getElementById('btn_validar').addEventListener('click',(evt)=>{
+    let msg = null
+
+    if(!f_nota.checkValidity()){
+        msg = f_nota.validationMessage
+    }
+
+    f_msg.innerHTML = msg
+    evt.preventDefault
+})
