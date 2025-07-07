37cf0bd8c86d7fae8ca348b5e0f44740ea1f9597
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-07-07 20:02:57 -0300

    Melhorando o código do desafio!!!

--- New_exercicios/Ex0033/meuscript.js
+++ New_exercicios/Ex0033/meuscript.js
@@ -1,13 +1,17 @@
-let n_clicada = 3
+let n_clicada = 4
 function receber_valores(n,arr_n){
-    let lampada_b = false 
     if(n%2==0){
         console.log(0)
     }else{
         console.log(1)
     }
+    let lam_02 = arr_n.filter(item=>item === 2).length
+    console.log(lam_02%2==0?'0':'1')
+}
+receber_valores(n_clicada,[2,1,2,2])
 
-    for(let i =0;i<arr_n.length;i++){
+    //let lampada_b = false 
+    /*for(let i =0;i<arr_n.length;i++){
         if(arr_n[i]===2){
             if(lampada_b ===false){
                 lampada_b = true
@@ -15,7 +19,6 @@ function receber_valores(n,arr_n){
                 lampada_b = false
             }
         }
-    }
-    console.log(lampada_b==true?'1':'0')
-}
-receber_valores(n_clicada,[1,2,2])
+    }*/
+
+    //console.log(lampada_b==true?'1':'0')
