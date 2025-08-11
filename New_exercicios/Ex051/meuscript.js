0957413d47e3d96a5fab862ca14c9e2e70191946
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-08-11 14:10:04 -0300

    atualizando

---
+++ New_exercicios/Ex051/meuscript.js
@@ -0,0 +1,10 @@
+const arr = [1,7,6,5,4]
+console.log("Array não ordenado"+arr)
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length-1;j++){
+       if(arr[i]>arr[j]){
+         
+       }
+    }
+}
+console.log("Array ordenado"+arr)
